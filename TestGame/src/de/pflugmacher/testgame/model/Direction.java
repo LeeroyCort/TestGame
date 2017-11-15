0593c01db182028942c16df363065217758b2950
Luca Pflugmacher <luca.pflugmacher@nomadrail.com>
2017-11-15 09:10:14 +0100

    Fixed direction bug and added curve function

--- TestGame/src/de/pflugmacher/testgame/model/Direction.java
+++ TestGame/src/de/pflugmacher/testgame/model/Direction.java
@@ -2,29 +2,33 @@ package de.pflugmacher.testgame.model;
 
 public class Direction {
 	private double glob_degrees;
+	private double traveledPixels;
+	private double path_length;
+	private boolean doCurve = false;
+	private double curveAngle;
 	
 	public Direction() {
-		glob_degrees = -90.0;
+		glob_degrees = 90.0;
 	}
 	
 	public Direction(double degrees) {
-		glob_degrees = degrees % 360;
+		glob_degrees = (degrees % 360) * -1;
 	}
 	
 	public void changeDegreesFromCurrent(double degrees, boolean right) {
 		if (right) {
-			glob_degrees -= degrees % 360;
+			glob_degrees -= (degrees % 360) * -1;
 		} else  {
-			glob_degrees += degrees % 360;
+			glob_degrees += (degrees % 360) * -1;
 		}		
 	}
 	
 	public void changeDegreesFromCurrent(double degrees) {
-		glob_degrees += degrees % 360;
+		glob_degrees += (degrees % 360) * -1;
 	}
 
 	public void changeDegreesFromGlobal(double degrees) {
-		glob_degrees = degrees % 360;
+		glob_degrees = (degrees % 360) * -1;
 	}
 	
 	public double getDegrees() {
@@ -36,8 +40,23 @@ public class Direction {
 	}
 	
 	public Position nextPositon(Position pos, double step) {
+		if (this.doCurve) {
+			if (traveledPixels >= path_length) {
+				this.doCurve = false;
+			} else {
+				this.traveledPixels += step;
+				changeDegreesFromCurrent(curveAngle / (path_length / step));
+			}
+		}
 		pos.x += Math.cos(Math.toRadians(glob_degrees)) * step;
 		pos.y += Math.sin(Math.toRadians(glob_degrees)) * step;
 		return pos;
+	}
+	
+	public void doCurve(double degrees, double path_length) {
+		this.doCurve = true;
+		this.traveledPixels = 0;
+		this.path_length = path_length;
+		this.curveAngle = degrees;
 	}
 }
